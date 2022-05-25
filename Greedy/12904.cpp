736d177c5af0ba4ecd5e9b9af2eb50770be1da68
Ko-GyeongTae <kokt0203@naver.com>
2022-05-25 09:07:30 +0900

    Feat(solve): solve

---
+++ Greedy/12904.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string S, T;
+    bool result = false;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> S >> T;
+
+    while (true)
+    {
+        if (S.length() == T.length())
+        {
+            if (S == T)
+            {
+                result = true;
+            }
+            break;
+        }
+        // T -> S가 성립하는가?
+        char tmp = T[T.length() - 1];
+        T.pop_back();
+        if (tmp == 'B')
+        {
+            reverse(T.begin(), T.end());
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
